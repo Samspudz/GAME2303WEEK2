9529dfb8d51ec438a16e1ea93d74ff692d4a4b51
Samspudz <adammarshall11@yahoo.com>
2023-09-12 00:27:11 -0500

    Update GAME2303WEEK2.cpp

--- GAME2303WEEK2.cpp
+++ GAME2303WEEK2.cpp
@@ -1,20 +1,41 @@
-// GAME2303WEEK2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+int division(int a, int b)
+{
+	int n = 1000;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (i % a == 0 && i % b == 0)
+		{
+			return
+		}
+	}
+}
+
+string reversal(string a)
+{
+
+}
+
+string removal(string a)
+{
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int num1 = 5;
+	int num2 = 8;
+
+	string string1 = "hello world";
+
+	string string2 = "the cake is a lie";
+
+	int result1 = division(num1, num2);
+	string result2 = reversal(string1);
+	string result3 = removal(string2);
+
+	cout << 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
